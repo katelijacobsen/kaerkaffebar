12615ed5312b54e8fdd976d9736a9ea89a6111bc
katelijacobsen <katjamaehleke98@gmail.com>
2025-05-15 12:15:43 +0200

    Implement PopUp functionality: add open and close event listeners, integrate with RunningClubCard and update API handling for form submissions.

--- src/pages/api/clubevents.js
+++ src/pages/api/clubevents.js
@@ -1,6 +1,7 @@
 // Importere moduler og supabase
 import { supabase } from "../../lib/supabase";
 
+export const prerender = false;
 //
 
 export async function GET() {
@@ -24,3 +25,28 @@ export async function GET() {
     {status: 200, headers: { "Content-Type": "application/json" } }
   )
 }
+
+
+// API route for at håndtere forespørgsel fra form
+export async function POST ({ request, redirect }) {
+  console.log(request);
+
+  const formData = await request.formData();
+
+  // Data
+  const data = {
+    fullName: formData.get("name"),
+    phoneNumber: formData.get("number"),
+  };
+  //error håndtering - hvis der skulle være fejl i dataen
+  const { error } = await supabase.from("request").insert([data]);
+  if (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+  // Retur hvis forespørgsel er sendt korrekt
+  //return new Response(JSON.stringify({ message: "success" }), { status: 200 });
+  // Bruger query string til at sende success tilbage til arrangement siden. Hvis brugeren trykker på send uden errors ville url'en vise true
+  return redirect("/runningclub?success=true");
+};
