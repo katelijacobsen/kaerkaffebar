0f0a31ebfa135e0189e601b1b7c866202041f919
katelijacobsen <katjamaehleke98@gmail.com>
2025-05-16 19:02:46 +0200

    reverse

--- src/pages/api/request.ts
+++ src/pages/api/request.ts
@@ -2,7 +2,7 @@
 import type { APIRoute } from "astro";
 import { supabase } from "../../lib/supabase";
 
-export const prerender = true;
+export const prerender = false;
 
 // API route for at håndtere forespørgsel fra form
 export const POST: APIRoute = async ({ request, redirect }) => {
