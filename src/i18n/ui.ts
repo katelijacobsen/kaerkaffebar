750d4adae17efab257dd4caec1f7a2e9b1f23a45
katelijacobsen <katjamaehleke98@gmail.com>
2025-05-17 12:18:43 +0200

    add files for i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+    da: 'Dansk',
+    en: 'English',
+};
+
+export const defaultLang = 'da';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.twitter': 'Twitter',
+  },
+  da: {
+    'nav.menu': 'Menu',
+    'nav.arrangement': 'Arrangement',
+  },
+} as const;
