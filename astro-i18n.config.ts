124fec2f01d8e83a787840c63e03cdb57e0ca870
katelijacobsen <katjamaehleke98@gmail.com>
2025-05-17 20:48:01 +0200

    push

--- astro-i18n.config.ts
+++ astro-i18n.config.ts
@@ -1,14 +1,19 @@
-import { defineAstroI18nConfig } from "astro-i18n"
+import { defineAstroI18nConfig } from "astro-i18n";
 
 export default defineAstroI18nConfig({
-	primaryLocale: "da", // default app locale
-	secondaryLocales: ["en"], // other supported locales
-	fallbackLocale: "da", // fallback locale (on missing translation)
-	trailingSlash: "never", // "never" or "always"
-	run: "client+server", // "client+server" or "server"
-	showPrimaryLocale: false, // "/en/about" vs "/about"
-	translationLoadingRules: [], // per page group loading
-	translationDirectory: {}, // translation directory names
-	translations: {}, // { [translation_group1]: { [locale1]: {}, ... } }
-	routes: {}, // { [secondary_locale1]: { about: "about-translated", ... } }
-})
+  primaryLocale: "da", // default app locale
+  secondaryLocales: ["en"], // other supported locales
+  fallbackLocale: "da", // fallback locale (on missing translation)
+  trailingSlash: "never", // "never" or "always"
+  run: "client+server", // "client+server" or "server"
+  showPrimaryLocale: false, // "/en/about" vs "/about"
+  translationLoadingRules: [], // per page group loading
+  translationDirectory: "i18n", // translation directory names
+  translations: {
+    common: {
+      da: () => import("./src/i18n/common/da.json"),
+      en: () => import("./src/i18n/common/en.json"),
+    },
+  }, // { [translation_group1]: { [locale1]: {}, ... } }
+  routes: {}, // { [secondary_locale1]: { about: "about-translated", ... } }
+});
